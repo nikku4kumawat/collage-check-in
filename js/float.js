0f530e4d093c9e587b21f950bb7228a4b9075b53
nikku4kumawat <nikkukumawat4@gmail.com>
2026-05-16 15:44:29 +0530

    updated project

--- js/float.js
+++ js/float.js
@@ -1,13 +1,14 @@
 const floatHTML = `
-    
+
     <div class="floating-container">
 
         <!-- CALL BUTTON -->
         <a
         href="tel:+919950000686"
         class="float-btn call-btn"
+        aria-label="Call Now"
         >
-            📞
+            <i class="fa-solid fa-phone"></i>
         </a>
 
         <!-- WHATSAPP BUTTON -->
@@ -15,19 +16,19 @@ const floatHTML = `
         href="https://wa.me/919950000686"
         target="_blank"
         class="float-btn whatsapp-btn"
+        aria-label="Chat on WhatsApp"
         >
-            💬
+            <i class="fa-brands fa-whatsapp"></i>
         </a>
 
     </div>
 
 `;
 
-
 /* =========================
 ADD FLOAT BUTTON
 ========================= */
 
 document.getElementById(
-"ss-float-placeholder"
+    "ss-float-placeholder"
 ).innerHTML = floatHTML;
